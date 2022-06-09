407086067a203cab85092491643d07def9cb7eab
ChouBeihai <chouBeihai@qq.com>
2022-06-09 21:56:23 +0800

    add 50

---
+++ algorithm/leetcode/company/microsoft/50/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x float64 = 2
+	var n = -2
+	fmt.Println(myPow(x, n))
+}
+
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	var flag = 1
+	if n < 0 {
+		flag = -1
+		n = -n
+	}
+	var res float64 = 1
+	var m = 1
+	var tmpRes = x
+	for n != 0 {
+		if n > m {
+			n = n - m
+		} else {
+			m = 1
+			tmpRes = x
+			n = n - m
+		}
+		res = res * tmpRes
+		tmpRes = tmpRes * tmpRes
+		m = m * 2
+	}
+	if flag == 1 {
+		return res
+	} else {
+		return 1 / res
+	}
+}
