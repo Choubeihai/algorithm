73ee4de10d2ec1c1945669e9b7d98f232b96aec1
ChouBeihai <choubeihai@qq.com>
2022-06-29 17:32:48 +0800

    update 51

--- algorithm/jianzhiOffer/51/main.go
+++ algorithm/jianzhiOffer/51/main.go
@@ -1,41 +1,50 @@
 package main
 
 func reversePairs(nums []int) int {
-	return mergeSort(nums, 0, len(nums)-1)
+	return sort(nums, 0, len(nums)-1)
 }
 
-func mergeSort(nums []int, left, right int) int {
+func sort(nums []int, left, right int) int {
 	if left >= right {
 		return 0
 	}
 	mid := (left + right) / 2
-	leftCount := mergeSort(nums, left, mid)
-	rightCount := mergeSort(nums, mid+1, right)
-	count := leftCount + rightCount
-	var i = left
-	var j = mid + 1
-	var tmp []int
-	for i <= mid && j <= right {
-		if nums[i] <= nums[j] {
-			tmp = append(tmp, nums[i])
-			count += j - (mid + 1)
+	leftCount := sort(nums, left, mid)
+	rightCount := sort(nums, mid+1, right)
+	return leftCount + rightCount + merge(nums, left, mid, right)
+}
+
+func merge(nums []int, left, mid, right int) int {
+	var arr1 = make([]int, mid-left+1)
+	var arr2 = make([]int, right-mid)
+	copy(arr1, nums[left:mid+1])
+	copy(arr2, nums[mid+1:right+1])
+	var i = 0
+	var j = 0
+	var k = left
+	var count = 0
+	for i < len(arr1) && j < len(arr2) {
+		if arr1[i] <= arr2[j] {
+			nums[k] = arr1[i]
+			k++
 			i++
-		} else {
-			tmp = append(tmp, nums[j])
+
+		} else { // arr1[i] > arr2[j]
+			nums[k] = arr2[j]
+			k++
 			j++
+			count += (len(arr1) - 1) - i + 1
 		}
 	}
-	for i <= mid {
-		tmp = append(tmp, nums[i])
-		count += right - (mid + 1) + 1
+	for i < len(arr1) {
+		nums[k] = arr1[i]
 		i++
+		k++
 	}
-	for j <= right {
-		tmp = append(tmp, nums[j])
+	for j < len(arr2) {
+		nums[k] = arr2[j]
 		j++
-	}
-	for k := left; k <= right; k++ {
-		nums[k] = tmp[k-left]
+		k++
 	}
 	return count
 }
