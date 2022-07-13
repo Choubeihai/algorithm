45e99a1dd92b5616abdd6f79391b88f505006717
ChouBeihai <chouBeihai@qq.com>
2022-07-13 22:47:29 +0800

    update 12

--- algorithm/leetcode/company/microsoft/12/main.go
+++ algorithm/leetcode/company/microsoft/12/main.go
@@ -13,8 +13,9 @@ func main() {
 func intToRoman(num int) string {
 	var nums = []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}                    // 罗马数字总共13个数字
 	var rom = []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"} // 对应的罗马数字
+	var n = len(rom)
 	var res = &strings.Builder{}
-	for i := 0; i < len(rom); i++ {
+	for i := 0; i < n; i++ {
 		for num >= nums[i] {
 			res.WriteString(rom[i])
 			num = num - nums[i]
