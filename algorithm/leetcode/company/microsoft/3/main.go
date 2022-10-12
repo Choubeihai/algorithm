cb7928fbc0bd16bb0896e4c04bd2abe7e3352ac2
ChouBeihai <choubeihai@qq.com>
2022-10-12 15:48:12 +0800

    add updates

--- algorithm/leetcode/company/microsoft/3/main.go
+++ algorithm/leetcode/company/microsoft/3/main.go
@@ -5,10 +5,8 @@ func lengthOfLongestSubstring(s string) int {
 	start := 0
 	res := 0
 	for i := 0; i < len(s); i++ {
-		if v, ok := m[s[i]]; ok {
-			if start < v+1 {
-				start = v + 1
-			}
+		if oldIdx, ok := m[s[i]]; ok && start < oldIdx+1 {
+			start = oldIdx + 1
 		}
 		m[s[i]] = i
 		if i-start+1 > res {
