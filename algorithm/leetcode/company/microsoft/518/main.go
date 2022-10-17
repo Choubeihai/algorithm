c816b4e81cf8ac86ef0eae022447b0a82c1ad9b9
ChouBeihai <chouBeihai@qq.com>
2022-10-17 22:22:46 +0800

    add some updates

--- algorithm/leetcode/company/microsoft/518/main.go
+++ algorithm/leetcode/company/microsoft/518/main.go
@@ -3,10 +3,11 @@ package main
 func change(amount int, coins []int) int {
 	dp := make([]int, amount+1)
 	dp[0] = 1
+
 	for i := 0; i < len(coins); i++ {
 		coin := coins[i]
-		for j := coin; j <= amount; j++ {
-			dp[j] += dp[j-coin]
+		for j := coins[i]; j <= amount; j++ {
+			dp[j] = dp[j] + dp[j-coin]
 		}
 	}
 	return dp[amount]
--- algorithm/leetcode/company/microsoft/60/main.go
+++ algorithm/leetcode/company/microsoft/60/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "bytes"
+import "strconv"
 
 /**
 一开始的思路是使用全排列做，但是时间超时了。
@@ -9,25 +9,26 @@ import "bytes"
 
 func getPermutation(n int, k int) string {
 	var fact = make([]int, n+1) // 记录阶乘
-	fact[0] = 1
+	fact[0] = 1                 // 0的阶乘是1
 	for i := 1; i <= n; i++ {
 		fact[i] = fact[i-1] * i
 	}
-	var res = &bytes.Buffer{}
+	var res string
 	var used = make([]bool, n+1) // 表明哪些数字被使用过
-	for i := n; i >= 1; i-- {    // 欲选的排列中还有几个数字没被确定
+
+	for i := 1; i <= n; i++ { // 欲选的排列中还有几个数字没被确定
 		cnt := fact[n-i]
 		for j := 1; j <= n; j++ { // 判断数字
 			if !used[j] {
 				if k > cnt {
 					k = k - cnt
 				} else {
-					res.WriteByte(byte(j + '0'))
+					res += strconv.Itoa(j)
 					used[j] = true
 					break
 				}
 			}
 		}
 	}
-	return res.String()
+	return res
 }
